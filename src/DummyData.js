2d9a667f9b5e80a290ee751613a62b1e1babaaea
marikomands <marikomands@gmail.com>
2023-07-10 20:32:52 +0100

    created data

---
+++ src/DummyData.js
@@ -0,0 +1,22 @@
+export const PostsData = [
+  {
+    id: 1,
+    name: "ShinCode",
+    content: "Studying Redux",
+  },
+  {
+    id: 2,
+    name: "Anonymous",
+    content: "Coding is hard",
+  },
+  {
+    id: 3,
+    name: "Junior Enginner",
+    content: "I might want to take some courses from Udemy",
+  },
+  {
+    id: 4,
+    name: "Mariko",
+    content: "Learning how to code sometimes frustrates me",
+  },
+];
